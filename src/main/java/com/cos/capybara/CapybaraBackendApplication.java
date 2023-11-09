e797932d69d5690a58f0d2e81242212535b58907
Joris Schwetke <joris.schwetke@sybit.de>
2023-11-09 13:44:46 +0100

    feat(): aufsetzen des Project

---
+++ src/main/java/com/cos/capybara/CapybaraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cos.capybara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapybaraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapybaraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/capybara/CapybaraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.capybara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapybaraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
